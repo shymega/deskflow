cc9d7e5dbb9f3a6df5b5ff3be033c87c3528936b
sithlord48 <sithlord48@gmail.com>
2024-10-31 14:47:40 -0400

    build: remove DESKFLOW_SHOW_DEV_THANKS define

--- src/lib/gui/config/AppConfig.cpp
+++ src/lib/gui/config/AppConfig.cpp
@@ -89,7 +89,7 @@ const char *const AppConfig::m_SettingsName[] = {
     "closeToTray",
     "mainWindowSize",
     "mainWindowPosition",
-    "showDevThanks",
+    "", // 41 = Show dev thanks, obsolete
     "showCloseReminder",
     "enableUpdateCheck",
 };
@@ -155,7 +155,6 @@ void AppConfig::recallFromCurrentScope()
   m_MainWindowPosition =
       getFromCurrentScope<QPoint>(kMainWindowPosition, [](const QVariant &v) { return v.toPoint(); });
   m_MainWindowSize = getFromCurrentScope<QSize>(kMainWindowSize, [](const QVariant &v) { return v.toSize(); });
-  m_ShowDevThanks = getFromCurrentScope(kShowDevThanks, m_ShowDevThanks).toBool();
   m_ShowCloseReminder = getFromCurrentScope(kShowCloseReminder, m_ShowCloseReminder).toBool();
   m_EnableUpdateCheck = getFromCurrentScope<bool>(kEnableUpdateCheck, [](const QVariant &v) { return v.toBool(); });
 }
@@ -213,7 +212,6 @@ void AppConfig::commit()
     setInCurrentScope(kCloseToTray, m_CloseToTray);
     setInCurrentScope(kMainWindowSize, m_MainWindowSize);
     setInCurrentScope(kMainWindowPosition, m_MainWindowPosition);
-    setInCurrentScope(kShowDevThanks, m_ShowDevThanks);
     setInCurrentScope(kShowCloseReminder, m_ShowCloseReminder);
     setInCurrentScope(kEnableUpdateCheck, m_EnableUpdateCheck);
   }
@@ -574,11 +572,6 @@ std::optional<QPoint> AppConfig::mainWindowPosition() const
   return m_MainWindowPosition;
 }
 
-bool AppConfig::showDevThanks() const
-{
-  return m_ShowDevThanks;
-}
-
 bool AppConfig::showCloseReminder() const
 {
   return m_ShowCloseReminder;
@@ -750,11 +743,6 @@ void AppConfig::setMainWindowSize(const QSize &size)
 void AppConfig::setMainWindowPosition(const QPoint &position)
 {
   m_MainWindowPosition = position;
-}
-
-void AppConfig::setShowDevThanks(bool value)
-{
-  m_ShowDevThanks = value;
 }
 
 void AppConfig::setShowCloseReminder(bool value)
